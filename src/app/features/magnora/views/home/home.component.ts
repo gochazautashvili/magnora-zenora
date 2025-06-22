6d66d4de98ead9af767848170d445d771c3e35c5
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-22 20:00:18 +0400

    chore

--- src/app/features/magnora/views/home/home.component.ts
+++ src/app/features/magnora/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeHeroSectionComponent } from '../../components/home-sections/home-hero-section/home-hero-section.component';
 import { HomeServiceSectionComponent } from '@features/magnora/components/home-sections/home-service-section/home-service-section.component';
+import { HomeQaSectionComponent } from '@features/magnora/components/home-sections/home-qa-section/home-qa-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHeroSectionComponent, HomeServiceSectionComponent],
+  imports: [HomeHeroSectionComponent, HomeServiceSectionComponent, HomeQaSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
