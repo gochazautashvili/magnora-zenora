68e42832646a7e6a8133186c83fa7ffdea4cd24c
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-22 15:23:35 +0400

    merge to g-m

--- src/app/features/magnora/views/home/home.component.ts
+++ src/app/features/magnora/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeHeroSectionComponent } from '../../components/home-sections/home-hero-section/home-hero-section.component';
+import { HomeServiceSectionComponent } from '@features/magnora/components/home-sections/home-service-section/home-service-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHeroSectionComponent],
+  imports: [HomeHeroSectionComponent, HomeServiceSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
