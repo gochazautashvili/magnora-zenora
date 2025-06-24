63d23b66a6ab07c10cc8c8ff94904b50459f1dd2
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-24 11:50:27 +0400

    chore:Added imports

--- src/app/features/magnora/views/home/home.component.ts
+++ src/app/features/magnora/views/home/home.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 
+// from components folder
 import {
   HomeQaSectionComponent,
   HomeHeroSectionComponent,
   HomeAboutSectionComponent,
-
-  
   HomeServiceSectionComponent,
   HomeFeatureSectionComponent,
-
   
 } from '@features/magnora/components/home-sections';
+
+// from layout folder
+import { FooterComponent } from '@features/magnora/layout/footer/footer.component';
+
+
+interface FooterComponentProps {
+  footerHeader: string;
+  footerContent: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -20,9 +27,22 @@ import {
     HomeAboutSectionComponent,
     HomeServiceSectionComponent,
     HomeFeatureSectionComponent,
-    HomeQaSectionComponent
+    HomeQaSectionComponent,
+    FooterComponent
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+
+  // Magnora footer texts
+  MagnorafooterTexts : FooterComponentProps = {
+    footerHeader : 'Magnora', 
+    footerContent : 'Experience timeless elegance, perfected in every detail.'
+  };
+  // Zenora footer texts
+  ZenorafooterTexts : FooterComponentProps = {
+    footerHeader : 'Zenora Footer Headers',
+    footerContent : 'Zenora Footer Content'
+  };
+}
