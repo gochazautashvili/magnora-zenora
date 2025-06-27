b6fcf7ee9cf7cbe768f23b48a1bf7864427b417e
GeorgeShani <shanidze.geo2004@gmail.com>
2025-06-27 15:55:40 +0400

    Create and configure contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ export const routes: Routes = [
   { path: '', loadChildren: () => import('./features/auth/auth.routes') },
   { path: '', loadChildren: () => import('./features/magnora/magnora.routes') },
   { path: '', loadChildren: () => import('./features/zenora/zenora.routes') },
+  { path: '', loadChildren: () => import('./features/contact/contact.routes') }
 ];
---
+++ src/app/shared/components/ora-contact-sections/contact-next-steps-section/contact-next-steps-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-next-steps-section',
+  imports: [],
+  templateUrl: './contact-next-steps-section.component.html',
+  styleUrl: './contact-next-steps-section.component.scss'
+})
+export class ContactNextStepsSectionComponent {
+
+}
