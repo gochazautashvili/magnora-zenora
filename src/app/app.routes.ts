89236155fc5d6a681d7910206f70c21210a39f81
nutsa nasaraia <nucanasaraia@gmail.com>
2025-06-27 16:01:40 +0400

    Create and configure about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
   { path: '', loadChildren: () => import('./features/auth/auth.routes') },
   { path: '', loadChildren: () => import('./features/magnora/magnora.routes') },
   { path: '', loadChildren: () => import('./features/zenora/zenora.routes') },
-  { path: '', loadChildren: () => import('./features/contact/contact.routes') }
+  { path: '', loadChildren: () => import('./features/contact/contact.routes') },
+  { path: '', loadChildren: () => import("./features/about/about.routes") }
 ];
---
+++ src/app/shared/components/ora-about-section/index.ts
@@ -0,0 +1,3 @@
+export * from "./about-hero-section/about-hero-section.component";
+export * from "./about-description-section/about-description-section.component";
+export * from "./about-cta-section/about-cta-section.component";
