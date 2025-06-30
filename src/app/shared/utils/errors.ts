cb103fb5743028c23013e2c59ca29696d2f15bf8
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-30 14:17:51 +0400

    feat: add password complexity and match validators for improved form validation

--- src/app/shared/utils/errors.ts
+++ src/app/shared/utils/errors.ts
@@ -9,9 +9,11 @@ export const getFormErrorMessage = (control: AbstractControl) => {
     return `Minimum length is ${control.errors?.['minlength']?.requiredLength}`;
   }
 
+
   if (control.hasError('maxlength')) {
     return `Maximum length is ${control.errors?.['maxlength']?.requiredLength}`;
   }
+
 
   if (control.hasError('pattern')) return 'Invalid format';
   if (control.hasError('email')) return 'Invalid email format';
---
+++ src/app/shared/utils/validators/form.validators.ts
@@ -0,0 +1,20 @@
+import { FormControl, Validators, ValidatorFn, AbstractControl, FormGroup, ReactiveFormsModule,  ValidationErrors } from '@angular/forms';
+
+export const passwordComplexityValidator: ValidatorFn = (control: AbstractControl) => {
+    const value = control.value || '';
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasNumber = /\d/.test(value);
+    const isValid = hasUpperCase && hasNumber;
+  
+    return isValid ? null : { passwordComplexity: true };
+};
+
+
+
+export const passwordMatchValidator: ValidationErrors = (group: AbstractControl)  => {
+    const password = group.get('password')?.value;
+    const confirm = group.get('confirmPassword')?.value;
+  
+    return password === confirm ? null : { passwordsMismatch: true };
+  }
+
