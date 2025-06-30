f4a53520be85118bd11bd105b9704635871ac3b0
GeorgeShani <shanidze.geo2004@gmail.com>
2025-06-30 13:55:03 +0400

    Create and implement main overlay component

---
+++ src/app/core/components/main-overlay/main-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-overlay',
+  imports: [],
+  templateUrl: './main-overlay.component.html',
+  styleUrl: './main-overlay.component.scss'
+})
+export class MainOverlayComponent {
+
+}
