ec8c4d36804acc622599da8c069ffcad4ab184a6
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-30 14:16:49 +0400

    feat: implement sign-in form with validation and styling

--- src/app/features/auth/views/sign-in-form/sign-in-form.component.ts
+++ src/app/features/auth/views/sign-in-form/sign-in-form.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { ButtonComponent, InputComponent } from '@shared/ui';
+import { FormInputComponent } from '@shared/ui/form-input/form-input.component';
+import { getFormErrorMessage } from '@shared/utils/errors';
+import { passwordComplexityValidator } from '@shared/utils/validators/form.validators';
+import { FormControl, Validators, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in-form',
-  imports: [],
+  imports: [FormInputComponent, ButtonComponent, RouterOutlet, ReactiveFormsModule],
   templateUrl: './sign-in-form.component.html',
   styleUrl: './sign-in-form.component.scss'
 })
 export default class SignInFormComponent {
 
+  public getErrorMessage = getFormErrorMessage;
+
+  protected form = new FormGroup<ILogInForm>({
+
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8),
+        passwordComplexityValidator,
+      ],
+    }),
+  })
+}
+
+
+interface ILogInForm{
+  email: FormControl<string>;
+  password: FormControl<string>;
 }
