937201a6cfbdc13b5e38f25b68a17b8993beca47
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-30 14:15:19 +0400

    feat: added sign-up view

--- src/app/features/auth/layout/sign-in/sign-in.component.ts
+++ src/app/features/auth/layout/sign-in/sign-in.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
