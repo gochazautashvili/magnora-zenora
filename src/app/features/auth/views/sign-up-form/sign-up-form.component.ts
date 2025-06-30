7ef5d9ec33978f4ce783ba26dbfe8b6abd5f98d6
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-06-30 14:17:15 +0400

    feat: enhance sign-up form with validation and improved layout

--- src/app/features/auth/views/sign-up-form/sign-up-form.component.ts
+++ src/app/features/auth/views/sign-up-form/sign-up-form.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '@shared/ui';
 import { FormInputComponent } from '@shared/ui/form-input/form-input.component';
 import { getFormErrorMessage } from '@shared/utils/errors';
+import { passwordComplexityValidator, passwordMatchValidator } from '@shared/utils/validators/form.validators';
+
+
+import { FormControl, Validators, ValidatorFn, AbstractControl, FormGroup, ReactiveFormsModule,  ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -12,38 +15,64 @@ import { getFormErrorMessage } from '@shared/utils/errors';
   styleUrl: './sign-up-form.component.scss'
 })
 
+
+
 export default class SignUpFormComponent {
+
+  // checks if the phone number is valid: only numbers and exactly 9 digits
+  private phoneNumberValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value || '';
+  
+    if (!/^\d*$/.test(value)) {
+      return { onlyNumbers: true };
+    }
+  
+    if (value.length !== 9) {
+      return { exactLength: true };
+    }
+  
+    return null;
+  }
+  
+
   public getErrorMessage = getFormErrorMessage;
   
   protected form = new FormGroup<ISignUpForm>({
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    lastName: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    number: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        this.phoneNumberValidator
+      ],
+    }),
 
-    firstName: new FormControl<string>('', {
-          nonNullable: true,
-          validators: Validators.required,
-      }),
-    lastName: new FormControl<string>('', {
-          nonNullable: true,
-          validators: Validators.required,
-      }),
-    email: new FormControl<string>('', {
-          nonNullable: true,
-          validators: [Validators.required, Validators.email],
-      }),
-    number: new FormControl<string>('', {
-          nonNullable: true,
-          validators: [Validators.required, Validators.pattern(/^\+995\d{9}$/)],
-      }),
-    password: new FormControl<string>('', {
-          nonNullable: true,
-          validators: [Validators.required, Validators.minLength(8)],
-      }),
-    confirmPassword: new FormControl<string>('', {
-          nonNullable: true,
-          validators: [Validators.required, Validators.minLength(8)],
-      }),
-  })
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8),
+        passwordComplexityValidator,
+      ],
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  }, { validators: passwordMatchValidator['bind'](this) });
+  
 }
-
 
 interface ISignUpForm {
   firstName: FormControl<string>;
