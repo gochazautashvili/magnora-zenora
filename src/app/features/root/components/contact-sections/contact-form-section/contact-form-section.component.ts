a8bb9b54fc9920d18eb3c341f80fa94b9088a48f
GeorgeShani <shanidze.geo2004@gmail.com>
2025-07-02 14:11:35 +0400

    Modify contact form

--- src/app/features/root/components/contact-sections/contact-form-section/contact-form-section.component.ts
+++ src/app/features/root/components/contact-sections/contact-form-section/contact-form-section.component.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
+import { FormInputComponent } from '../../../../../shared/ui/form-input/form-input.component';
+import { ButtonComponent } from '../../../../../shared/ui/button/button.component';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-section',
-  imports: [],
+  imports: [FormInputComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './contact-form-section.component.html',
-  styleUrl: './contact-form-section.component.scss'
+  styleUrl: './contact-form-section.component.scss',
 })
 export class ContactFormSectionComponent {
+  protected contactForm = new FormGroup<IContactForm>({
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(60),
+      ],
+    }),
+    lastName: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(60),
+      ],
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    phoneNumber: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.pattern('^\\+[1-9]\\d{1,14}$'),
+      ],
+    }),
+    message: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(50)],
+    }),
+  });
 
+  onSubmit(): void {
+    // TODO: Implement logic for contact form submission
+  }
+}
+
+interface IContactForm {
+  firstName: FormControl<string>;
+  lastName: FormControl<string>;
+  email: FormControl<string>;
+  phoneNumber: FormControl<string>;
+  message: FormControl<string>;
 }
