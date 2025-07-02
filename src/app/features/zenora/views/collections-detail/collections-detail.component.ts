155b9a3626c83e6fc48f752ff07e7c30bdd52d57
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-02 14:25:28 +0400

    feat: add collections detail component with template and tests; enhance success message service

---
+++ src/app/features/zenora/views/collections-detail/collections-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-detail',
+  imports: [],
+  templateUrl: './collections-detail.component.html',
+  styleUrl: './collections-detail.component.scss'
+})
+export default class CollectionsDetailComponent {
+
+}
