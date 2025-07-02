524718b28193b8b2327a78c3db0c62adf0c72ca5
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-02 12:18:19 +0400

    feat: add collection details products component with template and test

---
+++ src/app/shared/components/mz-collections-details-secitons/collection-details-products/collection-details-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-details-products',
+  imports: [],
+  templateUrl: './collection-details-products.component.html',
+  styleUrl: './collection-details-products.component.scss'
+})
+export class CollectionDetailsProductsComponent {
+
+}
