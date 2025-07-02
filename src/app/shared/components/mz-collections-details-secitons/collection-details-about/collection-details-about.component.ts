43c48d02c4a570c94dd4aadab7207ddf85f33ad9
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-02 12:17:32 +0400

    feat: add collection details about component with template, styles, and tests

---
+++ src/app/shared/components/mz-collections-details-secitons/collection-details-about/collection-details-about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-details-about',
+  imports: [],
+  templateUrl: './collection-details-about.component.html',
+  styleUrl: './collection-details-about.component.scss'
+})
+export default class CollectionDetailsAboutComponent {
+
+}
