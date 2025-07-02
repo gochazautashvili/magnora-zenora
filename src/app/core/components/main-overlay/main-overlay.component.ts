25ecb7c703eaa76fe7f628af1f2639ab2afce159
GeorgeShani <shanidze.geo2004@gmail.com>
2025-07-02 16:11:37 +0400

    Implement search bar section

--- src/app/core/components/main-overlay/main-overlay.component.ts
+++ src/app/core/components/main-overlay/main-overlay.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearchSectionComponent } from "../search-section/search-section.component";
 
 @Component({
   selector: 'app-main-overlay',
-  imports: [CommonModule],
+  imports: [CommonModule, SearchSectionComponent],
   templateUrl: './main-overlay.component.html',
   styleUrl: './main-overlay.component.scss'
 })
