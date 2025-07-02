04907a6f60337a8a4ffb768b47c974bf0f90d19f
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-02 12:16:19 +0400

    feat: update success message component with new layout, styles, and message handling

--- src/app/features/auth/views/succsess-message/succsess-message.component.ts
+++ src/app/features/auth/views/succsess-message/succsess-message.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SuccessMessageService } from '@features/auth/services/success-message-services/success-message.service';
 
 @Component({
   selector: 'app-succsess-message',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './succsess-message.component.html',
   styleUrl: './succsess-message.component.scss'
 })
-export class SuccsessMessageComponent {
+export default class SuccsessMessageComponent implements OnInit {
+  successMessageService = inject(SuccessMessageService)
+  
+  message: string = '';
+  ngOnInit(): void {
+    this.successMessageService.successMessage$.subscribe({
+      next: (message) => {
+        this.message = message
+      },
+      error: (err) => {
+        console.error('Error receiving success message:', err);
+      }
+    })
+  }
+
 
 }
