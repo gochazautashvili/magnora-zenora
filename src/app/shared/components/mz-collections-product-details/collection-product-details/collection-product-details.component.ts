cce2f8b5a155e8a70ced3ad14f86491f81bd0402
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-03 21:42:25 +0400

    feat: implement dynamic product details and navigation functionality

--- src/app/shared/components/mz-collections-product-details/collection-product-details/collection-product-details.component.ts
+++ src/app/shared/components/mz-collections-product-details/collection-product-details/collection-product-details.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ButtonComponent } from '@shared/ui';
 import { BookConsultComponent } from "../../book-consult/book-consult.component";
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@shared/services/product-services/products.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-collection-product-details',
@@ -8,6 +12,26 @@ import { BookConsultComponent } from "../../book-consult/book-consult.component"
   templateUrl: './collection-product-details.component.html',
   styleUrl: './collection-product-details.component.scss'
 })
-export default class CollectionProductDetailsComponent {
+export default class CollectionProductDetailsComponent implements OnInit {
+  id!: string;
+product: any;
+
+route = inject(ActivatedRoute);
+productService = inject(ProductsService);
+
+ngOnInit() {
+  window.scrollTo(0, 0);
+  this.id = this.route.snapshot.paramMap.get('id')!;
+
+  this.product = this.productService.products.find(
+    product => product.id === parseInt(this.id, 10)
+  );
+}
+constructor(private location: Location) {}
+
+goBack() {
+  this.location.back();
+} 
+
 
 }
