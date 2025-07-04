799ebacbaee8da20a6c56ae5932a562db31a3bd3
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-04 13:27:44 +0400

    feat: enhance reset password component with translation support and improved layout

--- src/app/features/auth/layout/reset-password/reset-password.component.ts
+++ src/app/features/auth/layout/reset-password/reset-password.component.ts
@@ -1,12 +1,18 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, RouterModule],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export default class ResetPasswordComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
