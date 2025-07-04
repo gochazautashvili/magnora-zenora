10d07c70fa4d387bca373c05ac7843e23022d00a
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-04 21:12:34 +0400

    feat: add success message component for sign-up form

---
+++ src/app/features/auth/views/sign-up-form/succsess-message/succsess-message.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import SuccessSecitonComponent from '@features/auth/components/success-seciton/success-seciton.component';
+import { ButtonComponent } from '@shared/ui';
+
+
+@Component({
+  selector: 'app-succsess-message',
+  imports: [SuccessSecitonComponent],
+  templateUrl: './succsess-message.component.html',
+  styleUrl: './succsess-message.component.scss'
+})
+export default class SuccsessMessageComponent {
+
+
+
+
+}
