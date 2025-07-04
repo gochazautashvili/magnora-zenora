b8c4aca9cbf79367f3efc0a15584c96ef7bca631
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-04 13:27:53 +0400

    feat: enhance sign-in component with translation support and improved layout

--- src/app/features/auth/layout/sign-in/sign-in.component.ts
+++ src/app/features/auth/layout/sign-in/sign-in.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, RouterModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrl: './sign-in.component.scss',
 })
-export default class SignInComponent {
-
-}
-
-
+export default class SignInComponent {}
