b18b737554f241c0c635f5199779058bf9a6ffbf
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2025-07-04 13:27:33 +0400

    feat: add search section component with input and results display

---
+++ src/app/core/components/search-section/search-section.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { OverlayService } from '@shared/services/overlay.service';
+
+@Component({
+  selector: 'app-search-section',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './search-section.component.html',
+  styleUrl: './search-section.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(8px)' }),
+        animate(
+          '200ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
+})
+export class SearchSectionComponent implements OnInit {
+  @Input() isMobile!: boolean;
+  @Input() type!: string;
+
+  topValue!: number;
+  searchQuery: string = '';
+
+  constructor(public overlayService: OverlayService) {}
+
+  ngOnInit(): void {
+    this.topValue = this.isMobile ? 24 : 172;
+  }
+
+  closeOverlaySearchBar(): void {
+    this.overlayService.toggleOverlay(false);
+  }
+}
